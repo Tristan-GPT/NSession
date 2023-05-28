f29619ad9be9025884246575240fe8adb0875928
Tristan <127656581+Tristan-GPT@users.noreply.github.com>
2023-05-28 11:38:07 +0200

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -19,6 +19,8 @@ caracteres = [
     "4","5","6","7","8","9","0",
 
   ]
+
+session = []
   
   
 def sessiontoken_gen():
@@ -31,9 +33,23 @@ def sessiontoken_gen():
 
     return password  
 
-class Client:
-    def createClient(self, name = str, id = int):
+class Session:
+    def createSession(self, name = str, id = int):
         self.name = name
         self.id = id
         self.sessionToken = sessiontoken_gen()
+        self.data = []
+        session.insert(0, {"name": self.name, "id": self.id, "sessionToken": self.sessionToken, 'data': []})
 
+ses = Session()
+ses.createSession(name="session1", id="session1-user-data")
+
+def saveData(newData):
+  try:
+     sess = session[0]
+     data = sess['data']
+     data.insert(0, newData)
+     print(data)
+     
+  except:
+    print("Error")
